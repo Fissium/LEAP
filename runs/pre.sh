fbd6d6e8c04c1a2f87a8aea19251780d87cd527e
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2024-07-03 14:14:16 +0300

    fix .sh

--- runs/pre.sh
+++ runs/pre.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=10:00:00
 
 WORK_DIR="/tmp/2024-05-10_LEAP"
-INIT_DIR="/mnt/pool/5/ovkomleva/LEAP"
+SOURCE_DIR="/mnt/pool/5/ovkomleva/LEAP"
 
 mkdir -p "$WORK_DIR/data"
+mkdir -p "$WORK_DIR/logs"
 chmod 700 "$WORK_DIR"
-cp -r "$INIT_DIR/data/train" "$WORK_DIR/data"
-cp "$INIT_DIR/data/test.csv" "$WORK_DIR/data"
-cp "$INIT_DIR/data/sample_submission.csv" "$WORK_DIR/data"
-cp -r "$INIT_DIR/.venv" "$WORK_DIR"
+cp -r "$SOURCE_DIR/data/train" "$WORK_DIR/data"
+cp "$SOURCE_DIR/data/test.csv" "$WORK_DIR/data"
+cp "$SOURCE_DIR/data/sample_submission.csv" "$WORK_DIR/data"
+cp -r "$SOURCE_DIR/.venv" "$WORK_DIR"
 cp -r /mnt/pool/5/ovkomleva/metric_learning/miniforge3 "$WORK_DIR"
--- runs/run.sh
+++ runs/run.sh
@@ -9,23 +9,24 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --time=48:00:00
 
-WORKDIR="/tmp/2024-05-10_LEAP"
-CONDA_DIR="$WORKDIR/miniforge3/bin/activate"
-RUN_SCRIPT="$WORKDIR/src/run.py"
-VENV_NAME="$WORKDIR/.venv"
+WORK_DIR="/tmp/2024-05-10_LEAP"
+SOURCE_DIR="/mnt/pool/5/ovkomleva/LEAP/"
+CONDA_DIR="$WORK_DIR/miniforge3/bin/activate"
+RUN_SCRIPT="$WORK_DIR/src/run.py"
+VENV_NAME="$WORK_DIR/.venv"
 LOGS_DIR="/mnt/pool/5/ovkomleva/LEAP/logs"
-EXCLUDE_DIRS=("data" "logs" "notebooks" "runs")
+EXCLUDE_DIRS=("data" "logs" "notebooks" "runs" ".venv" ".git")
 
-mkdir -p "$WORKDIR/logs"
+mkdir -p "$WORK_DIR/logs"
 
-rsync -av --exclude="${EXCLUDE_DIRS[*]}" . "$WORKDIR"
+rsync -av --exclude="${EXCLUDE_DIRS[*]}" "$SOURCE_DIR" "$WORK_DIR"
 
-cd $WORKDIR || exit 1
+cd $WORK_DIR || exit 1
 source "$CONDA_DIR" "$VENV_NAME"
 
 if python $RUN_SCRIPT; then
 
-  LATEST_LOG_DIR=$(ls -td "$WORKDIR/logs"/*/ | head -1)
+  LATEST_LOG_DIR=$(ls -td "$WORK_DIR/logs"/*/ | head -1)
   if [ -d "$LATEST_LOG_DIR" ]; then
     cp -r "$LATEST_LOG_DIR" "$LOGS_DIR"
   else
