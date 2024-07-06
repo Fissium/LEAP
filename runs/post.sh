bdd41a6f47e52e7df5e5a4ffc192a69a7dc52a02
Nikita Rubinkovskiy <narubinkovskiy@gmail.com>
2024-07-06 18:38:17 +0300

    Hotfix

--- runs/post.sh
+++ runs/post.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --time=1:00:00
 
-WORK_DIR="/tmp/2024-05-10_LEAP"
+WORK_DIR="/tmp/2024-05-10_LEAP/logs"
 LOG_DIR="2024-07-06_18-05-38"
 SOURCE_DIR="/mnt/pool/6/ovkomleva/LEAP"
 
